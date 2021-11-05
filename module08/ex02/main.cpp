ec8f139fb41f8344ca9a36527d69579f0a286a68
zqadiri <zqadiri@student.1337.ma>
2021-11-05 13:21:10 +0100

    ex02 module 08

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,12 +6,13 @@
 /*   By: zqadiri <zqadiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 14:08:22 by zqadiri           #+#    #+#             */
-/*   Updated: 2021/11/04 18:48:23 by zqadiri          ###   ########.fr       */
+/*   Updated: 2021/11/05 13:20:24 by zqadiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
 
+/*LIFO*/
 int main()
 {
 	//Creating a Class Template Object : className<dataType> classObject;
@@ -24,17 +25,18 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
+	mstack.push(77);
+	mstack.push(37);
 	mstack.push(0);
-	// MutantStack<int>::iterator it = mstack.begin();
-	// MutantStack<int>::iterator ite = mstack.end();
-	// ++it;
-	// --it;
-	// while (it != ite)
-	// {
-	// std::cout << *it << std::endl;
-	// ++it;
-	// }
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
 	std::stack<int> s(mstack);
 	return 0;
 }
--- module08/ex02/mutantstack.hpp
+++ module08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: zqadiri <zqadiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 18:14:06 by zqadiri           #+#    #+#             */
-/*   Updated: 2021/11/04 18:59:56 by zqadiri          ###   ########.fr       */
+/*   Updated: 2021/11/05 13:16:00 by zqadiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <iterator>
 
 template <typename T>
 class MutantStack : public std::stack<T>
@@ -29,11 +30,18 @@ class MutantStack : public std::stack<T>
 			*this = mutant;
 		}
 		MutantStack<T> &operator=(const MutantStack<T> &mutant){
-			*this = mutant;
+			(void)mutant;
 			return (*this);
 		}
-		
 
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator	begin(void){
+			return this->c.begin();
+		}
+		iterator	end(void){
+			return this->c.end();
+		}
 };
 
 #endif
