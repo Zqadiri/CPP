eec7bb356e570380e45b78b54ac811caebdac673
zqadiri <zqadiri@student.1337.ma>
2022-02-28 11:07:09 +0100

    05 done

--- module05/ex03/Intern.cpp
+++ module05/ex03/Intern.cpp
@@ -6,44 +6,53 @@
 /*   By: zqadiri <zqadiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 19:52:56 by zqadiri           #+#    #+#             */
-/*   Updated: 2022/02/27 20:21:31 by zqadiri          ###   ########.fr       */
+/*   Updated: 2022/02/28 11:06:28 by zqadiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
 
-Intern::Intern(/* args */)
-{
+Intern::Intern(){
+	std::cout << "Intern Default constructor" << std::endl;
 }
 
-Intern::~Intern()
-{
-
+Intern::~Intern(){
+	std::cout << "Intern Default Destructor" << std::endl;
 }
 
-// whose name is the one passed as parameter whose target will be initialized to the second parameter
-Form  *makeForm(std::string formName , std::string target)
+Form  *Intern::makeForm(std::string formName , std::string target)
 {
-    // Intern creates <form>
-    std::string names[] = {"robotomy request","shrubbery creation","presidential pardon"};
-	Form* (Intern::*fct[])(std::string) = 
+	int i = 0;
+    std::string requests[] = {"robotomy request","shrubbery creation","presidential pardon"}; // array of requests
+	Form* (Intern::*fct[])(std::string) = {&Intern::RobotomyRequest, &Intern::ShrubberyCreation, &Intern::PresidentialPardon};
+	for (i = 0; i < 3; i++)
 	{
-		&Intern::RobotomyRequest, &Intern::ShrubberyCreation, &Intern::PresidentialPardon
-	};
-    
+		if (!formName.compare(requests[i])){
+			std::cout << "Intern creates " << requests[i] << std::endl;
+			break;
+		}
+	}
+	if (i == 3){
+		std::cout << "Form Not Found" << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	return (this->*fct[i])(target);
 }
 
-Form*	PresidentialPardon(std::string name)
+Form*	RobotomyRequest(std::string target)
 {
-
+	RobotomyRequestForm *ret = new RobotomyRequestForm(target);
+	return ret;
 }
 
-Form*	ShrubberyCreation(std::string name)
+Form*	PresidentialPardon(std::string target)
 {
-    
+	PresidentialPardonForm *ret = new PresidentialPardonForm(target);
+	return ret;
 }
 
-Form*	RobotomyRequest(std::string name)
+Form*	ShrubberyCreation(std::string target)
 {
-    
+    ShrubberyCreationForm *ret = new ShrubberyCreationForm(target);
+	return ret;
 }
--- module05/ex03/Intern.hpp
+++ module05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: zqadiri <zqadiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 19:52:00 by zqadiri           #+#    #+#             */
-/*   Updated: 2022/02/27 20:13:56 by zqadiri          ###   ########.fr       */
+/*   Updated: 2022/02/28 10:50:07 by zqadiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ class Intern
 		Intern();
 		~Intern();
 		Intern( Intern const & src );
-		Form*	Makeform(std::string form,std::string name);
+		Form*	makeForm(std::string form,std::string name);
 		Form*	PresidentialPardon(std::string name);
 		Form*	ShrubberyCreation(std::string name);
 		Form*	RobotomyRequest(std::string name);
